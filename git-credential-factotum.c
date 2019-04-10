b6fabcfe4b22025d2526272749ef7e3a306b47b5
Philippe MECHAI <philippe.mechai@gmail.com>
2019-04-10 10:07:35 +0200

    Implement 'store' operation
    	Storage is done through factotum ctl file from the local namespace.
    	No error checking is done as git-credential does not expect error messages
    	in any way.

--- git-credential-factotum.c
+++ git-credential-factotum.c
@@ -5,50 +5,92 @@
 #include <thread.h>
 #include <bio.h>
 
-char *svc;
-char *srv;
+typedef struct Request Request;
 
-void
+struct Request
+{
+	char *service;
+	char *server;
+	char *user;
+	char *passwd;
+};
+
+Request*
 parserequest()
 {
+	Request *req;
 	Biobuf buf;
 	char *line, *args[6];
 	int i, n;
 
+	req = malloc(sizeof(Request));
+	if(!req)
+		sysfatal("malloc");
 	Binit(&buf, 0, OREAD);
-	for(;;){
-		line = Brdline(&buf, '\n');
-		if(!line)
-			break;
+	while((line = Brdstr(&buf, '\n', 1))){
 		n = gettokens(line, args, 6, "=\n");
 		for(i = 0; i < n; i = i+2){
 			if(!strcmp(args[i], "protocol")){
-				svc = args[i+1];
+				req->service = strdup(args[i+1]);
 			}else if(!strcmp(args[i], "host")){
-				srv = args[i+1];
+				req->server = strdup(args[i+1]);
+			}else if(!strcmp(args[i], "username")){
+				req->user = strdup(args[i+1]);
+			}else if(!strcmp(args[i], "password")){
+				req->passwd = strdup(args[i+1]);
 			}
 		}
-		if(svc == nil || srv == nil)
-			threadexits(nil);
+		free(line);
 	}
 	Bterm(&buf);
+	return req;
+}
+
+void
+get(Request *r)
+{
+	UserPasswd *up;
+
+	if(!r->service || !r->server)
+		return;
+	
+	up = auth_getuserpasswd(auth_getkey, "proto=pass role=client service=%s server=%s", r->service, r->server);
+	if(!up)
+		return;
+
+	print("username=%s\n", up->user);
+	print("password=%s\n", up->passwd);
+}
+
+void
+store(Request *r)
+{
+	CFid *fid;
+	char *key;
+
+	if(!r->service || !r->server || !r->user || !r->passwd)
+		return;
+
+	key = smprint("key proto=pass service=%s server=%s user=%s !password=%s\n",
+					r->service, r->server, r->user, r->passwd);
+	fid = nsopen("factotum", nil, "ctl", OWRITE);
+	if(!fid)
+		sysfatal("opening factotum/ctl: %r");
+	fswrite(fid, key, strlen(key));
+	fsclose(fid);
 }
 
 void
 threadmain(int argc, char *argv[])
 {
-	UserPasswd *up;
+	Request *r;
 
 	if(argc == 1)
 		threadexits("args");
-	if(strcmp(argv[1], "get") != 0)
-		threadexits(nil);
-	svc = nil;
-	srv = nil;
-	parserequest();	
-	up = auth_getuserpasswd(auth_getkey, "proto=pass role=client service=%s server=%s", svc, srv);
-	if(up){
-		print("username=%s\n", up->user);
-		print("password=%s\n", up->passwd);
-	}
+	
+	r = parserequest();
+	if(!strcmp(argv[1], "get"))
+		get(r);
+	else if(!strcmp(argv[1], "store"))
+		store(r);
 }
